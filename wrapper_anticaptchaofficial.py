9d23bc98f8e0d4ba6f41219d38236c654b41f22b
Douglas Jerônimo <douglaswwaff@gmail.com>
2023-12-23 00:39:42 -0400

    add hcaptcha no wrapper anticaptchaofficial

--- wrapper_anticaptchaofficial.py
+++ wrapper_anticaptchaofficial.py
@@ -1,6 +1,7 @@
 from .controller import get_key # TODO remove it "." to run locally
 from anticaptchaofficial import (
     imagecaptcha,
+    hcaptchaproxyless,
 )
 
 
@@ -14,5 +15,18 @@ def imgcaptcha(img: str) -> str | None:
     captcha_text = solver.solve_and_return_solution(img)
     if captcha_text != 0:
         return captcha_text
+
+    return None
+
+def hcaptcha(url: str, sitekey: str, invisible: bool = False) -> str | None:
+    solver = hcaptchaproxyless.hCaptchaProxyless()
+    solver.set_key(API_KEY)
+    solver.set_website_url(url)
+    solver.set_website_key(sitekey)
+    solver.set_is_invisible(invisible)
+
+    g_response = solver.solve_and_return_solution()
+    if g_response != 0:
+        return g_response
 
     return None
