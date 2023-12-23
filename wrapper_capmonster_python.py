9e4ef69f228d34c449b1e62028998f36a8fe2065
Douglas Jerônimo <douglaswwaff@gmail.com>
2023-12-23 00:47:12 -0400

    add hcaptcha no wrapper capmonster_python

--- wrapper_capmonster_python.py
+++ wrapper_capmonster_python.py
@@ -1,5 +1,12 @@
 from .controller import get_key # TODO remove it "." to run locally
+from capmonster_python import HCaptchaTask
 
 
 KEY_NAME = 'CAPMONSTER_PYTHON'
 API_KEY  = get_key(KEY_NAME)
+
+def hcaptcha(url: str, sitekey: str, invisible: bool = False) -> str | None:
+    capmonster = HCaptchaTask(API_KEY)
+    task = capmonster.create_task(url, sitekey, is_invisible=invisible)
+
+    return capmonster.join_task_result(task).get('gRecaptchaResponse')
