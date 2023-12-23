4b3b7a34570b20a2f4d59c001780622b4f766ea5
Douglas Jerônimo <douglaswwaff@gmail.com>
2023-12-23 00:38:33 -0400

    add imgcaptcha no wrapper anticaptchaofficial

--- wrapper_anticaptchaofficial.py
+++ wrapper_anticaptchaofficial.py
@@ -1,5 +1,18 @@
 from .controller import get_key # TODO remove it "." to run locally
+from anticaptchaofficial import (
+    imagecaptcha,
+)
 
 
 KEY_NAME = 'ANTI_CAPTCHA_OFFICIAL'
 API_KEY  = get_key(KEY_NAME)
+
+def imgcaptcha(img: str) -> str | None:
+    solver = imagecaptcha.imagecaptcha()
+    solver.set_key(API_KEY)
+
+    captcha_text = solver.solve_and_return_solution(img)
+    if captcha_text != 0:
+        return captcha_text
+
+    return None
