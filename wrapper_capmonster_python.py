031cdd14316d54645d795844cb70f66126476561
Douglas Jerônimo <douglaswwaff@gmail.com>
2023-12-23 00:50:31 -0400

    add recaptcha_v2 no wrapper capmonster_python

--- wrapper_capmonster_python.py
+++ wrapper_capmonster_python.py
@@ -1,6 +1,6 @@
 from .controller import get_key # TODO remove it "." to run locally
 from capmonster_python import HCaptchaTask
-from capmonstercloudclient.requests import HcaptchaProxylessRequest
+from capmonstercloudclient.requests import HcaptchaProxylessRequest, RecaptchaV2ProxylessRequest
 from capmonstercloudclient import ClientOptions, CapMonsterClient
 from asyncio import run
 
@@ -22,5 +22,16 @@ def hcaptcha2(url: str, sitekey: str) -> str | None:
 
     async def solve_captcha():
         return await client.solve_captcha(hcaptcha2request)
+
+    return run(solve_captcha()).get('gRecaptchaResponse')
+
+def recaptcha_v2(url: str, sitekey: str) -> str | None:
+    recaptcha2request = RecaptchaV2ProxylessRequest(websiteUrl=url, websiteKey=sitekey)
+
+    client_options = ClientOptions(api_key=API_KEY)
+    client = CapMonsterClient(options=client_options)
+
+    async def solve_captcha():
+        return await client.solve_captcha(recaptcha2request)
 
     return run(solve_captcha()).get('gRecaptchaResponse')
